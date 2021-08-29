c24a05010f4559bf9f73559fbf48e706328edad4
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-08-30 02:33:47 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  // console.log (bracketsConfig);
 
- let check = '';
-  bracketsConfig.forEach(element => {
-    if (element.includes('(')) { check ='true' }
-  });
-    // console.log(check);
 	
- const openBracketArr = ['(', '{', '['];
-  const bracketPair = {
-    [')']: '(',
-    ['}']: '{',
-	[']']: '['
-  }
+  let bracketPair ={};
+    
+  bracketsConfig.forEach(element => {
+    bracketPair[element[1]] = element[0];
+  });
+    
+  let openBracketArr = Object.values(bracketPair);
+  let closeBracketArr = Object.keys(bracketPair);
 
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
     let currentBracket = str[i];
-	// console.log (bracketsConfig.includes(currentBracket));
-    if (openBracketArr.includes(currentBracket) && check === 'true') {
+	  
+	  
+    if (openBracketArr.includes(currentBracket) ) {
       stack.push(currentBracket);
+		if (stack.length > 1 && (stack[stack.length-1] === stack[stack.length-2]) && closeBracketArr.includes(currentBracket) ) { stack.pop(); stack.pop();}
     } else {
       if (stack.length === 0) return false;
 
-      let topStackBracket = stack[stack.length - 1];
-      if (bracketPair[currentBracket] === topStackBracket) {
+		
+      if (bracketPair[currentBracket] === stack[stack.length - 1]) {
         stack.pop();
       } else {
         return false
       }
     }
       }
+	console.log(stack);
 return stack.length === 0;
+  
+
+
+
 
 }
