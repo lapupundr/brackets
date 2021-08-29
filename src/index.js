c9a26f12dd16b53cd433bed8fe743c3b416d57bc
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-08-30 01:05:55 +0300

    first way

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  // console.log (bracketsConfig);
+
+ let check = '';
+  bracketsConfig.forEach(element => {
+    if (element.includes('(')) { check ='true' }
+  });
+    // console.log(check);
+	
+ const openBracketArr = ['(', '{', '['];
+  const bracketPair = {
+    [')']: '(',
+    ['}']: '{',
+	[']']: '['
+  }
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+	// console.log (bracketsConfig.includes(currentBracket));
+    if (openBracketArr.includes(currentBracket) && check === 'true') {
+      stack.push(currentBracket);
+    } else {
+      if (stack.length === 0) return false;
+
+      let topStackBracket = stack[stack.length - 1];
+      if (bracketPair[currentBracket] === topStackBracket) {
+        stack.pop();
+      } else {
+        return false
+      }
+    }
+      }
+return stack.length === 0;
+
 }
